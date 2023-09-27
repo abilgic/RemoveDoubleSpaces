06d2ac90f8629c7e5653265b2774fbcb694d23a7
ahmet bilgic <ahmet_bilgic@yahoo.com>
2023-09-27 22:35:07 +0300

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var str = "Hello  World";
+var result = "";
+var stack = new Stack<char>();
+for (int i = 0; i < str.Length; i++)
+{
+    if (str[i].ToString().Equals(" "))
+    {
+        stack.Push(str[i]);
+    }
+
+    if (stack.Count == 0 || stack.Count == 1)
+    {        
+        result += str[i].ToString();        
+    }
+    else
+    {
+        stack.Pop();
+    }
+}
+Console.WriteLine(result);
+
